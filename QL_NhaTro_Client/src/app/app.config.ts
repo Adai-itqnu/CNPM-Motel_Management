19ee59dfbe90a0c4a56620e38c37b2b0f36a28ef
adai <trananhdai.itqnu@gmail.com>
2025-12-25 23:20:49 +0700

    update config

--- QL_NhaTro_Client/src/app/app.config.ts
+++ QL_NhaTro_Client/src/app/app.config.ts
@@ -1,19 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter, withRouterConfig } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([authInterceptor])
-    )
-  ]
+    provideRouter(routes, withRouterConfig({ onSameUrlNavigation: 'reload' })),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
